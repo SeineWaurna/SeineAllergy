d5dec273092fd7396d30f6a79fa0e8a4ab616938
Seinewaruna <seinewaruna@gmail.com>
2024-02-25 13:46:49 +0700

    face success

---
+++ lib/utility/app_constant.dart
@@ -0,0 +1,14 @@
+class AppConstant {
+  static List<String> pathFaces = <String>[
+    'assets/images/redface.png',
+    'assets/images/redface.png',
+    'assets/images/redface.png',
+    'assets/images/blueface.png',
+    'assets/images/blueface.png',
+    'assets/images/blueface.png',
+    'assets/images/blueface.png',
+    'assets/images/yellowface.png',
+    'assets/images/yellowface.png',
+    'assets/images/yellowface.png',
+  ];
+}
